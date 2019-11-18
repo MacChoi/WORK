9fb36c25c86c5ff30bb6e314393df3ae5050f2d3
choi <diskbox1@daum.net>
2019-11-18 16:48:31 +0900

    commit

--- KUNGFU/constant.js
+++ KUNGFU/constant.js
@@ -15,3 +15,42 @@ var OBJECT;
 var IMAGE;
 var SOUND;
 var STATE;
+
+function log(text){
+    if(GEngine.debug == false)return;
+    console.log(text);   
+}
+
+function sleep(delay){
+    var start =new Date().getTime();
+    while(new Date().getTime()< start + delay);
+}
+
+function objToString(obj){
+    JSON.stringify(obj);
+}
+
+function getRandom(a,b){
+    return Math.floor(Math.random() * b) + a;
+}
+
+function isEmpty(str){
+    if(typeof str == "undefined" || str == null || str == "")
+        return true;
+    else
+        return false ;
+}
+
+function getCircleXY(radius,angle,angleGap){
+    var arrayPosX = new Array(0);
+    var arrayPosY = new Array(0);
+    for (let index = angle; index > 0; index-=angleGap) {
+        var posX = radius * Math.sin(index * Math.PI/180);
+        var posY = radius * Math.cos(index * Math.PI/180);
+        
+        arrayPosX.push(parseInt(posX));
+        arrayPosY.push(parseInt(posY));
+    }
+    log("arrayPosX [" + arrayPosX.length +"] :" +  arrayPosX);
+    log("arrayPosY [" + arrayPosY.length +"] :" +  arrayPosY);
+}
